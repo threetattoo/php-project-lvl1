7684cff835b769482001ab45ac337e2db6b33533
Threetattoo <mikhailpazyuk@yandex.ru>
2019-12-26 15:46:45 +0500

    even game logic

---
+++ src/even.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BrainGames\Even;
+
+use function cli\line;
+use function cli\prompt;
+
+function numParity($num)
+{
+    return $num % 2 === 0;
+}
+
+$count = 0;
+
+function run()
+{
+    $correctAnswersCount = 3;
+    line('Welcome to Brain Games!');
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    do {
+        $randNum = mt_rand(1, 100);
+        line("Question: %s", $randNum);
+        $answer = prompt('Your answer: ');
+        $checkParity = (numParity($randNum)) ? 'yes' : 'no';
+        if ($answer === $checkParity) {
+            line("Correct!");
+            $count++;
+        } else {
+            line("{$answer} is wrong answer ;(. Correct answer was '$checkParity'.");
+        }
+    } while ($count < $correctAnswersCount);
+    line("Congratulations, %s!", $name);
+}
