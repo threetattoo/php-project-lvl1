3d6eea1133c7753be5da7bfbf5ef182b445cdd22
Threetattoo <mikhailpazyuk@yandex.ru>
2019-12-29 15:47:05 +0500

    step six

--- src/Cli.php
+++ src/Cli.php
@@ -2,8 +2,8 @@
 
 namespace BrainGames\Cli;
 
-use function Cli\line;
-use function Cli\prompt;
+use function cli\line;
+use function cli\prompt;
 
 function run()
 {
---
+++ src/games/calc.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BrainGames\Calc;
+
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+    $operators = array('+', '-', '*');
+    $count = 0;
+    $correctAnswersCount = 3;
+    line('Welcome to Brain Games!');
+    line('What is the result of the expression?');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    do {
+        $randIndex = array_rand($operators, 1);
+        $currentOperator = $operators[$randIndex];
+        $leftOperand = mt_rand(0, 50);
+        $rightOperand = mt_rand(0, 50);
+        switch ($currentOperator) {
+            case '+':
+                $currentTask = $leftOperand . '+' . $rightOperand;
+                $taskResult = $leftOperand + $rightOperand;
+                break;
+            case '-':
+                $currentTask = $leftOperand . '-' . $rightOperand;
+                $taskResult = $leftOperand - $rightOperand;
+                break;
+            case '*':
+                $currentTask = $leftOperand . '*' . $rightOperand;
+                $taskResult = $leftOperand * $rightOperand;
+                break;
+            default:
+                line('It is not operator!');
+        }
+        $currentTaskResult = (string) $taskResult;
+        line("Question: {$currentTask}");
+        $answer = prompt('Your answer: ');
+        if ($answer === $currentTaskResult) {
+            line("Correct!");
+            $count++;
+        } else {
+            line("{$answer} is wrong answer ;(. Correct answer was '{$currentTaskResult}'.");
+            line("Let's try again, %s!", $name);
+            break;
+        }
+    } while ($count < $correctAnswersCount);
+    line("Congratulations, %s!", $name);
+}
