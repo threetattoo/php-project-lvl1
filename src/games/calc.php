17267bb082543b12c85b46564dd40f9f429c9f9e
Threetattoo <mikhailpazyuk@yandex.ru>
2020-01-06 17:47:06 +0500

    coverted for work with core

--- src/games/calc.php
+++ src/games/calc.php
@@ -1,53 +1,34 @@
 <?php
 
-namespace BrainGames\Calc;
+namespace Brain\Games\Calc;
 
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\Core\gameProcessing;
 
 function run()
 {
-    $operators = array('+', '-', '*');
-    $count = 0;
-    $correctAnswersCount = 3;
-    line('Welcome to Brain Games!');
-    line('What is the result of the expression?');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-    do {
+    $gameRule = 'What is the result of the expression?';
+    $gameDataMaking = function () {
+        $operators = array('+', '-', '*');
         $randIndex = array_rand($operators, 1);
         $currentOperator = $operators[$randIndex];
         $leftOperand = mt_rand(0, 50);
         $rightOperand = mt_rand(0, 50);
         switch ($currentOperator) {
             case '+':
-                $currentTask = $leftOperand . '+' . $rightOperand;
-                $taskResult = $leftOperand + $rightOperand;
+                $result = $leftOperand + $rightOperand;
                 break;
             case '-':
-                $currentTask = $leftOperand . '-' . $rightOperand;
-                $taskResult = $leftOperand - $rightOperand;
+                $result = $leftOperand - $rightOperand;
                 break;
             case '*':
-                $currentTask = $leftOperand . '*' . $rightOperand;
-                $taskResult = $leftOperand * $rightOperand;
+                $result = $leftOperand * $rightOperand;
                 break;
             default:
                 line('It is not operator!');
         }
-        $currentTaskResult = (string) $taskResult;
-        line("Question: {$currentTask}");
-        $answer = prompt('Your answer: ');
-        if ($answer === $currentTaskResult) {
-            line("Correct!");
-            $count++;
-        } else {
-            line("{$answer} is wrong answer ;(. Correct answer was '{$currentTaskResult}'.");
-            line("Let's try again, %s!", $name);
-            break;
-        }
-    } while ($count < $correctAnswersCount);
-    if ($count == $correctAnswersCount) {
-        line("Congratulations, %s!", $name);
-    }
+        $gameQuestion = "$leftOperand $currentOperator $rightOperand";
+        $gameCorrectAnswer = (string) $result;
+        return [$gameQuestion, $gameCorrectAnswer];
+    };
+    gameProcessing($gameRule, $gameDataMaking);
 }
